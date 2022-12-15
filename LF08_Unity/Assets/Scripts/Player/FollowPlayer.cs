5a80a160411310f282817c7611109bc4f7758819
Sleepboop <73258412+Sleepboop@users.noreply.github.com>
2022-12-15 20:34:20 +0100

    Added Post Processing

--- LF08_Unity/Assets/Scripts/Player/FollowPlayer.cs
+++ LF08_Unity/Assets/Scripts/Player/FollowPlayer.cs
@@ -4,10 +4,10 @@ public class FollowPlayer : MonoBehaviour
 {
 
     public Transform player;
-
+    public float z = -5f;
     // Update is called once per frame
     void Update()
     {
-        transform.position = player.transform.position + new Vector3(0, 1, -5);
+        transform.position = player.transform.position + new Vector3(0, 1, z);
     }
 }
