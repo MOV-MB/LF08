791058a07e16eb141b55bddb22e0fd0ce0e0b7d2
MOV-MB <https://github.com/MOV-MB>
2022-12-18 15:05:11 +0100

    Improved EnemySpawner performance

--- LF08_Unity/Assets/EnemySpawner.cs
+++ LF08_Unity/Assets/EnemySpawner.cs
@@ -13,64 +13,112 @@ public class EnemySpawner : MonoBehaviour
     public float PurpleAlienInterval = 10f;
     public float Alien1Interval = 20f;
     
-    public int PoolSize = 10;    // size of the enemy pool
+    public int PoolSize = 5;    // size of the enemy pool
 
-    private Queue<GameObject> _spiderPool;
-    private Queue<GameObject> _purpleAlienPool;
-    private Queue<GameObject> _alien1Pool;
+    private List<GameObject> _spiderPool;
+    private List<GameObject> _purpleAlienPool;
+    private List<GameObject> _alien1Pool;
+
+    // Pre-determined positions for enemies to spawn at
+    private Vector3[] _spawnPositions;
 
     // Start is called before the first frame update
     void Start()
     {
         // Initialize the enemy pools
-        _spiderPool = new Queue<GameObject>(PoolSize);
-        _purpleAlienPool = new Queue<GameObject>(PoolSize);
-        _alien1Pool = new Queue<GameObject>(PoolSize);
+        _spiderPool = new List<GameObject>(PoolSize);
+        _purpleAlienPool = new List<GameObject>(PoolSize);
+        _alien1Pool = new List<GameObject>(PoolSize);
 
-        // Fill the pools with inactive enemy prefabs
+        // Pre-populate the enemy pools with inactive enemy prefabs
         for (int i = 0; i < PoolSize; i++)
         {
             GameObject spider = Instantiate(SpiderPrefab, Vector3.zero, Quaternion.identity);
             spider.SetActive(false);
-            _spiderPool.Enqueue(spider);
+            _spiderPool.Add(spider);
 
             GameObject purpleAlien = Instantiate(PurpleAlienPrefab, Vector3.zero, Quaternion.identity);
             purpleAlien.SetActive(false);
-            _purpleAlienPool.Enqueue(purpleAlien);
+            _purpleAlienPool.Add(purpleAlien);
 
             GameObject alien1 = Instantiate(Alien1Prefab, Vector3.zero, Quaternion.identity);
             alien1.SetActive(false);
-            _alien1Pool.Enqueue(alien1);
+            _alien1Pool.Add(alien1);
         }
 
-        // Start the enemy spawning coroutines
-        StartCoroutine(SpawnEnemy(SpiderInterval, _spiderPool, SpiderPrefab));
-        StartCoroutine(SpawnEnemy(PurpleAlienInterval, _purpleAlienPool, PurpleAlienPrefab));
-        StartCoroutine(SpawnEnemy(Alien1Interval, _alien1Pool, Alien1Prefab));
+        // Initialize the spawn positions array
+        _spawnPositions = new Vector3[]
+        {
+            new(Spawner.transform.position.x - 1f, Spawner.transform.position.y - 1f, 0f),
+            new(Spawner.transform.position.x + 1f, Spawner.transform.position.y - 1f, 0f),
+            new(Spawner.transform.position.x - 1f, Spawner.transform.position.y + 1f, 0f),
+            new(Spawner.transform.position.x + 1f, Spawner.transform.position.y + 1f, 0f)
+        };
+
+        // Start the enemy spawning coroutine
+        StartCoroutine(SpawnEnemies());
     }
 
-    private IEnumerator SpawnEnemy(float interval, Queue<GameObject> enemyPool, GameObject enemyPrefab)
+    private IEnumerator SpawnEnemies()
     {
-        yield return new WaitForSeconds(interval);
+        yield return new WaitForSeconds(SpiderInterval);
 
-        // Get the next enemy from the pool
-        GameObject enemy = enemyPool.Dequeue();
+        // Get the next spider from the pool
+        GameObject spider = GetNextEnemy(_spiderPool);
 
-        // If the pool is empty, add another enemy to it
-        if (enemy == null)
+        // Set the spider's position and activate it
+        spider.transform.position = GetRandomSpawnPosition();
+        spider.SetActive(true);
+
+        // Add the spider back to the pool
+        _spiderPool.Add(spider);
+
+        yield return new WaitForSeconds(PurpleAlienInterval);
+
+        // Get the next purple alien from the pool
+        GameObject purpleAlien = GetNextEnemy(_purpleAlienPool);
+
+        // Set the purple alien's position and activate it
+        purpleAlien.transform.position = GetRandomSpawnPosition();
+        purpleAlien.SetActive(true);
+
+        // Add the purple alien back to the pool
+        _purpleAlienPool.Add(purpleAlien);
+
+        yield return new WaitForSeconds(Alien1Interval);
+
+        // Get the next alien1 from the pool
+        GameObject alien1 = GetNextEnemy(_alien1Pool);
+
+        // Set the alien1's position and activate it
+        alien1.transform.position = GetRandomSpawnPosition();
+        alien1.SetActive(true);
+
+        // Add the alien1 back to the pool
+        _alien1Pool.Add(alien1);
+
+        StartCoroutine(SpawnEnemies());
+    }
+
+    // Returns the next enemy from the specified pool, or instantiates a new one if the pool is empty
+    private GameObject GetNextEnemy(IList<GameObject> enemyPool)
+    {
+        if (enemyPool.Count > 0)
         {
-            enemy = Instantiate(enemyPrefab, Vector3.zero, Quaternion.identity);
+            GameObject enemy = enemyPool[0];
+            enemyPool.RemoveAt(0);
+            return enemy;
         }
+        else
+        {
+            // Return a new enemy if the pool is empty
+            return Instantiate(SpiderPrefab, Vector3.zero, Quaternion.identity);
+        }
+    }
 
-        // Set the enemy's position and activate it
-        enemy.transform.position = new Vector3(Random.Range(Spawner.transform.position.x - 1f, Spawner.transform.position.x + 1f),
-                                               Random.Range(Spawner.transform.position.y - 1f, Spawner.transform.position.y + 1f),
-                                               0f);
-        enemy.SetActive(true);
-
-        // Add the enemy back to the pool
-        enemyPool.Enqueue(enemy);
-
-        StartCoroutine(SpawnEnemy(interval, enemyPool, enemyPrefab));
+    // Returns a random position from the spawn positions array
+    private Vector3 GetRandomSpawnPosition()
+    {
+        return _spawnPositions[Random.Range(0, _spawnPositions.Length)];
     }
 }
