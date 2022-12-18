53ffa5c3a3a497aedd3a3a5b68507aa48783572e
Sleepboop <73258412+Sleepboop@users.noreply.github.com>
2022-12-18 19:23:54 +0100

    Attempt to fix
    Attempt to fix
    
    Attempt to fix
    
    Attempt to fix
    
    Attempt to fix
    
    Attempt to fix

--- LF08_Unity/Assets/Scripts/Tests/EditMode/EditMode.cs
+++ LF08_Unity/Assets/Scripts/Tests/EditMode/EditMode.cs
@@ -5,31 +5,77 @@ using UnityEngine;
 using UnityEngine.TestTools;
 using Assets.Scripts.Player;
 
-public class EditMode : MonoBehaviour
+public class EditMode
 {
-    public Player Player = GameObject.Find("Player").GetComponent<Player>();
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
+    public Player player = new Player();
+=======
+    public Player Player = new();
+>>>>>>> parent of d696cfc (Fixed Unit Tests Locally)
+=======
+    public Player player = new Player();
+>>>>>>> parent of 19036c6 (Changed some assembly defs)
+=======
+    public Player player = new Player();
+>>>>>>> parent of cd4aea0 (Merge pull request #2 from MOV-MB/GameSceneMerge)
 
     [Test]
     public void PlayerBeginningStats()
     {
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> parent of 19036c6 (Changed some assembly defs)
+=======
+>>>>>>> parent of cd4aea0 (Merge pull request #2 from MOV-MB/GameSceneMerge)
+        Assert.AreEqual(false, player.GodMode);
+        Assert.AreEqual(true, player.CanMove);
+        Assert.AreEqual(30f, player.BulletForce);
+        Assert.AreEqual(0.5f, player.Firerate);
+        Assert.AreEqual(10f, player.MoveSpeed);
+        Assert.AreEqual(0, player.Money);
+        Assert.AreEqual(100f, player.Health);
+<<<<<<< HEAD
+<<<<<<< HEAD
+=======
         Assert.AreEqual(false, Player.GodMode);
         Assert.AreEqual(true, Player.CanMove);
         Assert.AreEqual(30f, Player.BulletForce);
-        Assert.AreEqual(0.3f, Player.Firerate);
-        Assert.AreEqual(8.5f, Player.MoveSpeed);
+        Assert.AreEqual(0.5f, Player.Firerate);
+        Assert.AreEqual(10f, Player.MoveSpeed);
         Assert.AreEqual(0, Player.Money);
         Assert.AreEqual(100f, Player.Health);
+>>>>>>> parent of d696cfc (Fixed Unit Tests Locally)
+=======
+>>>>>>> parent of 19036c6 (Changed some assembly defs)
+=======
+>>>>>>> parent of cd4aea0 (Merge pull request #2 from MOV-MB/GameSceneMerge)
     }
 
     [Test]
     public void HealthbarEqualToHealth()
     {
-        Assert.AreEqual(Player.Health, Player.HealthBar.healthSlider.value * 100);
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
+        Assert.AreEqual(player.Health, player.HealthBar.healthSlider.value / 100);
+=======
+        Assert.AreEqual(Player.Health, Player.HealthBar.healthSlider.value / 100);
+>>>>>>> parent of d696cfc (Fixed Unit Tests Locally)
+=======
+        Assert.AreEqual(player.Health, player.HealthBar.healthSlider.value / 100);
+>>>>>>> parent of 19036c6 (Changed some assembly defs)
+=======
+        Assert.AreEqual(player.Health, player.HealthBar.healthSlider.value / 100);
+>>>>>>> parent of cd4aea0 (Merge pull request #2 from MOV-MB/GameSceneMerge)
     }
 
     [Test]
     public void ShootingPointPlacement()
     {
-        Assert.IsTrue(Player.ShootingPoint.IsChildOf(Player.transform));
+        Assert.IsTrue(player.ShootingPoint.IsChildOf(player.transform));
     }
 }
