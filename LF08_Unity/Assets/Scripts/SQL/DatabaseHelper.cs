da9c28b935ba3743fd000cd103923beda746b28d
MOV-MB <https://github.com/MOV-MB>
2022-12-22 15:14:25 +0100

    Added Summaries

--- LF08_Unity/Assets/Scripts/SQL/DatabaseHelper.cs
+++ LF08_Unity/Assets/Scripts/SQL/DatabaseHelper.cs
@@ -4,8 +4,6 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Reflection;
-using System.Text;
-using System.Text.RegularExpressions;
 using UnityEngine;
 using Debug = UnityEngine.Debug;
 
@@ -20,6 +18,9 @@ public class DatabaseHelper : MonoBehaviour
         InitializeDatabase();
     }
 
+    /// <summary>
+    /// Method to Initialize the Database and call corresponding methods
+    /// </summary>
     private static void InitializeDatabase()
     {
         _devPathDB = "URI=file:" + "F:\\GitHubResp\\LF08\\LF08_Unity\\Assets\\DB\\Gamedb.db";
@@ -34,15 +35,23 @@ public class DatabaseHelper : MonoBehaviour
         dbConnection.Close();
     }
 
-    // ReSharper disable once InconsistentNaming
+    
+    /// <summary>
+    /// Creates the Database File if it doesn't exist already.
+    /// </summary>
     private static void CreateDBFile()
     {
-        if (File.Exists(_localPath)) return;
-        File.Create(_localPath);
+        if (File.Exists(_devPathDB)) return;
+        File.Create(_devPathDB);
 
         Debug.Log("DB File created");
     }
 
+
+    /// <summary>
+    /// Creates the database tables
+    /// </summary>
+    /// <param name="dbConnection">the database connection</param>
     private static void CreateTables(IDbConnection dbConnection)
     {
         if (dbConnection == null) return;
@@ -78,16 +87,29 @@ public class DatabaseHelper : MonoBehaviour
         
     }
 
+    /// <summary>
+    /// returns and establishes a database connection to the sqlite database
+    /// </summary>
+    /// <returns name="IDBConnection"></returns>
+
     private static IDbConnection CreateDbConnection()
     {
         Debug.Log("Establishing connection...");
-        IDbConnection dbConnection = new SqliteConnection(_pathDB);
+        IDbConnection dbConnection = new SqliteConnection(_devPathDB);
         dbConnection.Open();
         Debug.Log("Connection established");
 
         return dbConnection;
     }
 
+    /// <summary>
+    /// Generic Method to process select statements for the database
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="query"></param>
+    /// <param name="playerName"></param>
+    /// <param name="mapData"></param>
+    /// <returns>list of the passed through type</returns>
     public static List<T> ProcessSelectStatement<T>(string query, string playerName, Func<IDataReader, T> mapData)
     {
         IDbConnection dbConnection = CreateDbConnection();
@@ -121,6 +143,12 @@ public class DatabaseHelper : MonoBehaviour
         }
         return results;
     }
+
+    /// <summary>
+    /// Generic Method to process select statements for the database
+    /// </summary>
+    /// <param name="query">query to process</param>
+    /// <param name="parameters">parameters to replace</param>
     public static void ProcessInsertStatement(string query, Dictionary<string, object> parameters)
     {
         IDbConnection dbConnection = CreateDbConnection();
@@ -142,6 +170,12 @@ public class DatabaseHelper : MonoBehaviour
         dbConnection.Close();
     }
 
+    /// <summary>
+    /// Variation of ProcessingInsertStatements for basic insert statements
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="query"></param>
+    /// <param name="obj"></param>
     public static void ProcessBasicInsertStatement<T>(string query, T obj)
     {
         IDbConnection dbConnection = CreateDbConnection();
@@ -162,6 +196,13 @@ public class DatabaseHelper : MonoBehaviour
 
         dbConnection.Close();
     }
+
+    /// <summary>
+    /// Method to process generic update statements
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="query"></param>
+    /// <param name="obj"></param>
     public static void ProcessUpdateStatement<T>(string query, T obj)
     {
         IDbConnection dbConnection = CreateDbConnection();
